c1c7be4b35a7c76a249c5bf5347f3801e64e7af2
Glen Bangkila <hadeinvane@gmail.com>
2021-05-29 09:52:33 +0800

    Add icons in the footer side

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -89,16 +89,32 @@
                             <h2 class="tracking-wide font-sintony font-bold text-3xl text-white">{{ __('Our office') }}</h2>
 
                             <div class="mt-4">
-                                <p>{{ app('company')->addressLine1() }}</p>
-                                <p>{{ join(', ', [app('company')->address('city'), app('company')->address('state')]) . ' ' . app('company')->address('zip')  . ', ' . app('company')->address('country') }}</p>
+                                <div class="flex">
+                                    <svg class="w-6 h-6 inline-block mt-[2px]" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"></path>
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"></path>
+                                    </svg>
+                                    <div class="ml-2">
+                                        <p>{{ app('company')->addressLine1() }}</p>
+                                        <p>{{ join(', ', [app('company')->address('city'), app('company')->address('state')]) . ' ' . app('company')->address('zip')  . ', ' . app('company')->address('country') }}</p>
+                                    </div>
+                                </div>
                             </div>
 
-                            <p class="mt-2">
-                                <a href="mailto:{{ app('company')->get('email') }}" class="hover:text-white hover:underline">{{ app('company')->get('email') }}</a>
+                            <p class="mt-3 flex items-center">
+                                <svg class="w-6 h-6 inline-block" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"></path>
+                                </svg>
+
+                                <a href="mailto:{{ app('company')->get('email') }}" class="ml-2 hover:text-white hover:underline">{{ app('company')->get('email') }}</a>
                             </p>
 
-                            <p class="mt-2">
-                                <a href="tel:{{ app('company')->get('phone') }}" class="hover:text-white hover:underline">{{ app('company')->get('phone') }}</a>
+                            <p class="mt-3 flex items-center">
+                                <svg class="w-6 h-6 inline-block" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"></path>
+                                </svg>
+
+                                <a href="tel:{{ app('company')->get('phone') }}" class="ml-2 hover:text-white hover:underline">{{ app('company')->get('phone') }}</a>
                             </p>
                         </div>
                 
